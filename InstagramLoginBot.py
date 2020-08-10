2f909ec480debdf2ec75789abd988c0e0e3a324b
saicharan67 <mahankalisaicharan@gmail.com>
2020-08-10 17:25:26 +0530

    First

---
+++ InstagramLoginBot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome("C:\\Users\\hp\\Anaconda3\\chromedriver.exe")
+driver.get("https://www.instagram.com/accounts/login/")
+sleep(4)
+
+from selenium.webdriver.common.keys import Keys
+
+driver.find_element_by_xpath("//input[@name='username']").send_keys(" enter user name")
+driver.find_element_by_xpath("//input[@name='password']").send_keys( " enter password")
+sleep(2)
+driver.find_element_by_xpath("//button[@type='submit']").click()
